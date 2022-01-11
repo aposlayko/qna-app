ae6557cbf5ba1489aa09b8dd89ef226596703f59
Alexandr Poslayko <aposlayko@gmail.com>
2022-01-11 15:05:36 +0200

    add markdown-it

--- src/app/components/question-page/question-page.component.ts
+++ src/app/components/question-page/question-page.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Question} from '../../interfaces/question';
+import * as MarkdownIt from 'markdown-it';
+import {debounceTime, fromEvent, map} from "rxjs";
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-question-page',
   templateUrl: './question-page.component.html',
   styleUrls: ['./question-page.component.scss']
 })
-export class QuestionPageComponent implements OnInit {
-  question: Question;
+export class QuestionPageComponent implements OnInit, AfterViewInit {
+  @ViewChild('textAreaElement')
+  textAreaElement: ElementRef<HTMLTextAreaElement>;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  question: Question;
+  md = MarkdownIt();
+  textWithMarkdown: SafeHtml = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private sanitizer: DomSanitizer,
+  ) { }
 
   ngOnInit(): void {
     this.question = this.activatedRoute.snapshot.data['question'];
+    console.log(this.md.render('# markdown-it rulezz!'));
+
+
   }
 
+  ngAfterViewInit(): void {
+    fromEvent<KeyboardEvent>(this.textAreaElement.nativeElement, 'keyup')
+      .pipe(
+        map((event: KeyboardEvent) => {
+          return (<HTMLTextAreaElement>event.target).value;
+        }),
+        debounceTime(400)
+      ).subscribe(value => {
+      console.log(value);
+      this.textWithMarkdown = this.sanitizer.bypassSecurityTrustHtml(this.md.render(value));
+    });
+  }
 }
